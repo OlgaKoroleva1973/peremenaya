6e5cf5259e0d6351d4eabc421b0afa0b60f44536
OlgaKoroleva1973 <korolevaolga25071973@gmail.com>
2024-12-06 10:15:47 +0300

    Add files via upload

---
+++ Peremenaya.py
@@ -0,0 +1,5 @@
+Home_Work=12
+Number_of_hours=1.5
+Course_name='Python'
+Time_one_task=Number_of_hours/Home_Work
+print(Course_name,",всего задач:", Home_Work,",затрачено часов:",Number_of_hours,",среднее время выполнения",Time_one_task )
